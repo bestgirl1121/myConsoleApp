839c1492c62b7b2999073e5d5d9979181b390adc
Сергей Старченко < @DESKTOP-Q8JJKNE>
2024-03-17 17:59:50 +0300

    Изменение 1

--- myConsoleApp/Program.cs
+++ myConsoleApp/Program.cs
@@ -1,15 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace myConsoleApp
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // Ввод исходного массива строк с клавиатуры
+            Console.WriteLine("Введите элементы исходного массива строк (чтобы закончить, введите 'end'):");
+            string[] originalArray = ReadStringArray();
+
+            // Формирование нового массива из строк, длина которых <= 3
+            string[] newArray = FilterArray(originalArray);
+
+            // Вывод нового массива на экран
+            Console.WriteLine("\nНовый массив строк из элементов <= 3 символов:");
+            foreach (var item in newArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static string[] ReadStringArray()
+        {
+            const int initialSize = 5; // Начальный размер массива
+            string[] array = new string[initialSize];
+            int count = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "end" || count == array.Length)
+                {
+                    Array.Resize(ref array, count); // Обрезаем массив до фактического размера
+                    return array;
+                }
+                array[count] = input;
+                count++;
+                if (count == array.Length) Array.Resize(ref array, count * 2); // Увеличиваем размер массива, если он заполнен
+            }
+        }
+
+        static string[] FilterArray(string[] originalArray)
+        {
+            int count = 0;
+
+            // Подсчет количества строк, которые удовлетворяют условию
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                if (originalArray[i] != null && originalArray[i].Length <= 3)
+                {
+                    count++;
+                }
+            }
+
+            string[] newArray = new string[count];
+            count = 0;
+
+            // Заполнение нового массива со строками длиной <= 3 символов
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                if (originalArray[i] != null && originalArray[i].Length <= 3)
+                {
+                    newArray[count] = originalArray[i];
+                    count++;
+                }
+            }
+
+            return newArray;
         }
     }
 }
